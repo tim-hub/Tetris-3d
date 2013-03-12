d53a532e36c48a0b2ecc339123cdcab7223003be
Miłosz <m4rv1n@gazeta.pl>
2013-03-13 00:27:51 +0100

    Wersja z zatrzymaniem

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -3,16 +3,22 @@ using System.Collections;
 
 public class Controller : MonoBehaviour
 {
-    private float speed = 1.0f;
+    public float speed = 1.0f;
+    protected int go = 1;
     // Use this for initialization
     void Start()
     {
-
     }
-
+    void OnCollisionEnter()
+    {
+        go = 0;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, -speed * Time.deltaTime, 0f);
+        if (go == 1)
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0f);
+        }
     }
 }
